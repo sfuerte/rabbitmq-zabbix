cad01d511d860eb029617e08451b31637115aa88
sfuerte <aprivalov@wpcalmon01.wpninc.com>
2019-01-03 14:32:51 -0700

    option for disabling SSL cert verification

--- zabbix_agentd.scripts/rabbitmq_api.py
+++ zabbix_agentd.scripts/rabbitmq_api.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env /usr/bin/python
-'''Python module to query the RabbitMQ Management Plugin REST API and get
+'''
+Python module to query the RabbitMQ Management Plugin REST API and get
 results that can then be used by Zabbix.
-https://github.com/jasonmcintosh/rabbitmq-zabbix
+
+Forked from https://github.com/jasonmcintosh/rabbitmq-zabbix
+
+https://github.com/sfuerte/zbx-rabbitmq
 '''
 import json
 import optparse
 import socket
 import urllib2
+import ssl
 import subprocess
 import tempfile
 import os
-import logging
 
 
 class RabbitMQAPI(object):
     '''Class for RabbitMQ Management API'''
 
     def __init__(self, user_name='guest', password='guest', host_name='',
-                 port=15672, conf='/etc/zabbix/zabbix_agentd.conf', senderhostname=None, protocol='http'):
+                 port=15672, conf='/etc/zabbix/zabbix_agentd.conf', senderhostname=None, protocol='http', disable_cert_check=False):
         self.user_name = user_name
         self.password = password
         self.host_name = host_name or socket.gethostname()
@@ -25,16 +29,27 @@ class RabbitMQAPI(object):
         self.conf = conf or '/etc/zabbix/zabbix_agentd.conf'
         self.senderhostname = senderhostname
         self.protocol = protocol or 'http'
+        self.disable_cert_check = disable_cert_check
 
     def call_api(self, path):
         '''Call the REST API and convert the results into JSON.'''
         url = '{0}://{1}:{2}/api/{3}'.format(self.protocol, self.host_name, self.port, path)
         password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
         password_mgr.add_password(None, url, self.user_name, self.password)
-        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-        logging.debug('Issue a rabbit API call to get data on ' + path + " against " + self.host_name)
-	logging.debug('Full URL:' + url)
-        return json.loads(urllib2.build_opener(handler).open(url).read())
+        pass_handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+
+        ctx = ssl.create_default_context()
+
+        if self.protocol == 'https' and self.disable_cert_check:
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+
+        opener = urllib2.build_opener(pass_handler, urllib2.HTTPSHandler(context=ctx))
+        urllib2.install_opener(opener)
+        content = urllib2.urlopen(url).read()
+
+        return json.loads(content)
+
 
     def list_queues(self, filters=None):
         '''
@@ -45,7 +60,6 @@ class RabbitMQAPI(object):
         if not filters:
             filters = [{}]
         for queue in self.call_api('queues'):
-            logging.debug("Discovered queue " + queue['name'] + ", checking to see if it's filtered...")
             for _filter in filters:
                 check = [(x, y) for x, y in queue.items() if x in _filter]
                 shared_items = set(_filter.items()).intersection(check)
@@ -54,7 +68,6 @@ class RabbitMQAPI(object):
                                '{#VHOSTNAME}': queue['vhost'],
                                '{#QUEUENAME}': queue['name']}
                     queues.append(element)
-                    logging.debug('Discovered queue '+queue['vhost']+'/'+queue['name'])
                     break
         return queues
 
@@ -68,7 +81,6 @@ class RabbitMQAPI(object):
             filters = [{}]
         try:
             for shovel in self.call_api('shovels'):
-                logging.debug("Discovered shovel " + shovel['name'] + ", checking to see if it's filtered...")
                 for _filter in filters:
                     check = [(x, y) for x, y in shovel.items() if x in _filter]
                     shared_items = set(_filter.items()).intersection(check)
@@ -76,7 +88,6 @@ class RabbitMQAPI(object):
                         element = {'{#VHOSTNAME}': shovel['vhost'],
                                    '{#SHOVELNAME}': shovel['name']}
                         shovels.append(element)
-                        logging.debug('Discovered shovel '+shovel['vhost']+'/'+shovel['name'])
                         break
             return shovels
         except urllib2.HTTPError as err:
@@ -95,7 +106,6 @@ class RabbitMQAPI(object):
             element = {'{#NODENAME}': name,
                        '{#NODETYPE}': node['type']}
             nodes.append(element)
-            logging.debug('Discovered nodes '+name+'/'+node['type'])
         return nodes
 
     def check_queue(self, filters=None):
@@ -108,7 +118,6 @@ class RabbitMQAPI(object):
 
         for queue in self.call_api('queues'):
             success = False
-            logging.debug("Filtering out by " + str(filters))
             for _filter in filters:
                 check = [(x, y) for x, y in queue.items() if x in _filter]
                 shared_items = set(_filter.items()).intersection(check)
@@ -133,7 +142,6 @@ class RabbitMQAPI(object):
 
         for shovel in self.call_api('shovels'):
             success = False
-            logging.debug("Filtering out by " + str(filters))
             for _filter in filters:
                 check = [(x, y) for x, y in shovel.items() if x in _filter]
                 shared_items = set(_filter.items()).intersection(check)
@@ -144,7 +152,6 @@ class RabbitMQAPI(object):
                 key = '"rabbitmq.shovels[{0},shovel_{1},{2}]"'
                 key = key.format(shovel['vhost'], 'state', shovel['name'])
                 value = shovel.get('state', 0)
-                logging.debug("SENDER_DATA: - %s %s" % (key,value))
                 rdatafile.write("- %s %s\n" % (key, value))
 
 
@@ -160,16 +167,14 @@ class RabbitMQAPI(object):
             key = '"rabbitmq.queues[{0},queue_{1},{2}]"'
             key = key.format(queue['vhost'], item, queue['name'])
             value = queue.get(item, 0)
-            logging.debug("SENDER_DATA: - %s %s" % (key,value))
             tmpfile.write("- %s %s\n" % (key, value))
         ##  This is a non standard bit of information added after the standard items
         for item in ['deliver_get', 'publish', 'ack']:
             key = '"rabbitmq.queues[{0},queue_message_stats_{1},{2}]"'
             key = key.format(queue['vhost'], item, queue['name'])
             value = queue.get('message_stats', {}).get(item, 0)
-            logging.debug("SENDER_DATA: - %s %s" % (key,value))
             tmpfile.write("- %s %s\n" % (key, value))
-			
+
 
     def _send_data(self, tmpfile):
         '''Send the queue data to Zabbix.'''
@@ -178,18 +183,10 @@ class RabbitMQAPI(object):
             args = args + " -s " + self.senderhostname
         return_code = 0
         process = subprocess.Popen(args.format(self.conf, tmpfile.name),
-                                           shell=True, stdout=subprocess.PIPE,
-                                           stderr=subprocess.PIPE)
-        out, err = process.communicate()
-        logging.debug("Finished sending data")
+                                   shell=True, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        process.communicate()
         return_code = process.wait()
-        logging.info("Found return code of " + str(return_code))
-        if return_code == 1:
-            logging.error(out)
-            logging.error(err)
-        else:
-            logging.debug(err)
-            logging.debug(out)
         return return_code
 
     def check_aliveness(self):
@@ -199,27 +196,26 @@ class RabbitMQAPI(object):
     def check_server(self, item, node_name):
         '''First, check the overview specific items'''
         if item == 'message_stats_deliver_get':
-          return self.call_api('overview').get('message_stats', {}).get('deliver_get_details', {}).get('rate',0)
+            return self.call_api('overview').get('message_stats', {}).get('deliver_get_details', {}).get('rate', 0)
         elif item == 'message_stats_publish':
-          return self.call_api('overview').get('message_stats', {}).get('publish_details', {}).get('rate',0)
+            return self.call_api('overview').get('message_stats', {}).get('publish_details', {}).get('rate', 0)
         elif item == 'message_stats_ack':
-          return self.call_api('overview').get('message_stats', {}).get('ack_details', {}).get('rate',0)
+            return self.call_api('overview').get('message_stats', {}).get('ack_details', {}).get('rate', 0)
         elif item == 'message_count_total':
-          return self.call_api('overview').get('queue_totals', {}).get('messages',0)
+            return self.call_api('overview').get('queue_totals', {}).get('messages', 0)
         elif item == 'message_count_ready':
-          return self.call_api('overview').get('queue_totals', {}).get('messages_ready',0)
+            return self.call_api('overview').get('queue_totals', {}).get('messages_ready', 0)
         elif item == 'message_count_unacknowledged':
-          return self.call_api('overview').get('queue_totals', {}).get('messages_unacknowledged',0)
+            return self.call_api('overview').get('queue_totals', {}).get('messages_unacknowledged', 0)
         elif item == 'rabbitmq_version':
-          return self.call_api('overview').get('rabbitmq_version', 'None')
-        '''Return the value for a specific item in a node's details.'''
+            return self.call_api('overview').get('rabbitmq_version', 'None')
+
+        # Return the value for a specific item in a node's details.
         node_name = node_name.split('.')[0]
-        nodeInfo = self.call_api('nodes')
-        for nodeData in nodeInfo:
-            logging.debug("Checking to see if node name {0} is in {1} for item {2} found {3} nodes".format(node_name, nodeData['name'], item, len(nodeInfo)))
-            if node_name in nodeData['name'] or len(nodeInfo) == 1:
-                logging.debug("Got data from node {0} of {1} ".format(node_name, nodeData.get(item)))
-                return nodeData.get(item)
+        node_info = self.call_api('nodes')
+        for node_data in node_info:
+            if node_name in node_data['name'] or len(node_info) == 1:
+                return node_data.get(item)
         return 'Not Found'
 
 
@@ -232,6 +228,8 @@ def main():
     parser.add_option('--password', help='RabbitMQ API password', default='guest')
     parser.add_option('--hostname', help='RabbitMQ API host', default=socket.gethostname())
     parser.add_option('--protocol', help='Use http or https', default='http')
+    parser.add_option('--disable-cert-check', help='Disable SSL cert verification for https',
+                      default=False, action="store_true", dest="disable_cert_check",)
     parser.add_option('--port', help='RabbitMQ API port', type='int', default=15672)
     parser.add_option('--check', type='choice', choices=choices, help='Type of check')
     parser.add_option('--metric', help='Which metric to evaluate', default='')
@@ -239,18 +237,17 @@ def main():
     parser.add_option('--node', help='Which node to check (valid for --check=server)')
     parser.add_option('--conf', default='/etc/zabbix/zabbix_agentd.conf')
     parser.add_option('--senderhostname', default='', help='Allows including a sender parameter on calls to zabbix_sender')
-    parser.add_option('--logfile', help='File to log errors (defaults to /var/log/zabbix/rabbitmq_zabbix.log)', default='/var/log/zabbix/rabbitmq_zabbix.log')
+    parser.add_option('--logfile', help='File to log errors (defaults to /var/log/zabbix/rabbitmq_zabbix.log)',
+                      default='/var/log/zabbix/rabbitmq_zabbix.log')
     parser.add_option('--loglevel', help='Defaults to INFO', default='INFO')
     (options, args) = parser.parse_args()
     if not options.check:
         parser.error('At least one check should be specified')
-    logging.basicConfig(filename=options.logfile or "/var/log/zabbix/rabbitmq_zabbix.log", level=logging.getLevelName(options.loglevel or "INFO"), format='%(asctime)s %(levelname)s: %(message)s')
 
-    logging.debug("Started trying to process data")
     api = RabbitMQAPI(user_name=options.username, password=options.password,
                       host_name=options.hostname, port=options.port,
-                      conf=options.conf, senderhostname=options.senderhostname, 
-		     protocol=options.protocol)
+                      conf=options.conf, senderhostname=options.senderhostname,
+                      protocol=options.protocol, disable_cert_check=options.disable_cert_check)
     if options.filters:
         try:
             filters = json.loads(options.filters)
